3fdbb00fa54dd077bdcd30a39933ced633404aca
Nick Nanos <nicknanos7@gmail.com>
2024-12-22 17:20:40 +0200

    Add extra sensors and state func

--- game-code/player.js
+++ game-code/player.js
@@ -32,7 +32,7 @@ function spawnHero(){
 
 function spawnLizard(x,y){
     lizard = new Sprite(x, y);
-	lizard.w=20;
+	lizard.w=15;
 	lizard.h=24;
 	lizard.layer = 5;
 	lizard.rotationLock = 'true';
@@ -64,7 +64,7 @@ function spawnLizard(x,y){
 
 	//Sensor Sprite to detect when the player touches the ground
 	groundSensor = new Sprite();
-	groundSensor.w = lizard.w-5;
+	groundSensor.w = 15;
 	groundSensor.h = 5;
 	groundSensor.x = lizard.x;
 	groundSensor.y = lizard.y + lizard.h/2 + 2;
@@ -75,6 +75,46 @@ function spawnLizard(x,y){
 	g = new GlueJoint(lizard, groundSensor);
 	g.visible = false
 
+	//Left side sensor
+	leftSensor = new Sprite();
+	leftSensor.w = 2;
+	leftSensor.h = lizard.h - 3;
+	leftSensor.x = lizard.x - 10;
+	leftSensor.y = lizard.y;
+	leftSensor.mass = 0.0;
+	leftSensor.collider = 'none';
+	leftSensor.visible = false;
+	leftSensor.overlaps(allSprites);
+	l = new GlueJoint(lizard, leftSensor);
+	l.visible = false
+
+	//Right side sensor
+	rightSensor = new Sprite();
+	rightSensor.w = 2;
+	rightSensor.h = lizard.h - 3;
+	rightSensor.x = lizard.x + 10;
+	rightSensor.y = lizard.y;
+	rightSensor.mass = 0.0;
+	rightSensor.collider = 'none';
+	rightSensor.visible = false;
+	rightSensor.overlaps(allSprites);
+	r = new GlueJoint(lizard, rightSensor);
+	r.visible = false
+
+	//Top side sensor
+	topSensor = new Sprite();
+	topSensor.w = 15;
+	topSensor.h = 2;
+	topSensor.x = lizard.x;
+	topSensor.y = lizard.y - (lizard.h/2 + 0.5);
+	topSensor.mass = 0.0;
+	topSensor.collider = 'none';
+	topSensor.visible = false;
+	topSensor.overlaps(allSprites);
+	t = new GlueJoint(lizard, topSensor);
+	t.visible = false;
+
+
 	cameraSensor = new Sprite();
 	cameraSensor.w = 3;
 	cameraSensor.h = 3;
@@ -84,16 +124,55 @@ function spawnLizard(x,y){
 	cameraSensor.overlaps(allSprites);
 	cameraSensor.visible = false;
 
-
-
-
 	lizard.states = {
 		IDLE: 0,
 		WALK: 1,
 		JUMP: 2,
 		ATTACK: 3,
-		BLOCK: 4
+		BLOCK: 4,
+		STUCK: 5
 	};
 	lizard.currentState = lizard.states.IDLE;
+	lizard.prevState = lizard.states.IDLE;
 }
 
+function changeState(state) {
+	lizard.prevState = lizard.currentState;	
+	switch(state) {
+		case 0:
+		case 'IDLE':
+			lizard.currentState = lizard.states.IDLE;
+			break;
+		case 1:
+		case 'WALK':
+			lizard.currentState = lizard.states.WALK;
+			break;
+		case 2:
+		case 'JUMP':
+			lizard.currentState = lizard.states.JUMP;
+			break;
+		case 3:
+		case 'ATTACK':
+			lizard.currentState = lizard.states.ATTACK;
+			break;
+		case 4:
+		case 'BLOCK':
+			lizard.currentState = lizard.states.BLOCK;
+			break;
+		case 5:
+		case 'STUCK':
+			lizard.currentState = lizard.states.STUCK;
+			break;
+	}
+}
+
+//Checks if player is pushing against a platform
+//Used to prevent sprite bouncing and to controll the moving background
+function stuckCheck() {
+	if(leftSensor.overlapping(walkable) || rightSensor.overlapping(walkable)){
+		changeState('STUCK')
+	}
+	else if(!(leftSensor.overlapping(walkable) && rightSensor.overlapping(walkable))){
+		changeState('IDLE')
+	}
+}
