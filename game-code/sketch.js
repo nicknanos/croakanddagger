d5e4fee4f07c593e7621eb70e8c3279814d81ee6
Nick Nanos <nicknanos7@gmail.com>
2024-12-25 11:44:48 +0200

    Add soundFX, UI|Fix Sequencing

--- game-code/sketch.js
+++ game-code/sketch.js
@@ -19,6 +19,9 @@ let allSpritesGroup;
 
 let activePlayer;
 
+//Checks to see if player is in a movement sequence (prevents sequence cancellation)
+let inSequence = false;
+
 //Player Sensors
 let groundSensor,leftSensor,rightSensor,topSensor, cameraSensor;
 
@@ -39,16 +42,23 @@ let canAttack  = true;
 let attackSpeed = 300//ms
 
 //Sprites and Assets
-let hero, partner, witch, lizard, portal, hex, frog, fly, leaf;
+let hero, partner, witch, lizard, portal, hex, frog, enemies, fly, leaf;
 let heroImg, partnerImg, witchImg, lizardImg, portalImg, hexImg, frogImg, cloudImg, flyImg, leafImg;
 
-let forestTiles;
+let forestTiles, mountainTiles;
 
 //let cloudX = 0;
 
 //Intro Scene
 let floor, waitingRoom;
 let backgroundImg;
+
+//Sounds
+let forestMusic, coinSound;
+
+//ui
+let ui, heart
+let heartImg
 
 // background images object
 let forestBackground = [
@@ -78,9 +88,48 @@ let forestBackground = [
 	}
 ]
 
+let mountainBackground = [
+	{
+		file: './assets/backgrounds/mountain/sky.png',
+		img: undefined,
+		x: 0,
+		speed: 0	
+	},
+	{
+		file: './assets/backgrounds/mountain/city.png',
+		img: undefined,
+		x: 0,
+		speed: 0.2	
+	},
+	{
+		file: './assets/backgrounds/mountain/mountain1.png',
+		img: undefined,
+		x: 0,
+		speed: 0.4
+	},
+	{
+		file: './assets/backgrounds/mountain/mountain2.png',
+		img: undefined,
+		x: 0,
+		speed: 0.6
+	},
+	{
+		file: './assets/backgrounds/mountain/mountain3.png',
+		img: undefined,
+		x: 0,
+		speed: 0.7
+	}
+]
+
+let test
+
 //Preload assets
 function preload() {
 	for (let b of forestBackground){
+		b.img = loadImage(`${b.file}`)
+	}
+
+	for (let b of mountainBackground){
 		b.img = loadImage(`${b.file}`)
 	}
 
@@ -94,10 +143,16 @@ function preload() {
 
 	cloudImg =loadImage('./assets/backgrounds/forest/cloud.png');
 
-	forestTiles = loadImage('./assets/enviroment/forestTiles2.png')
-	coinsImg = loadImage('./assets/enviroment/coin.png')
-	flyImg = loadImage('./assets/enemies/forest/fly.png')
-	leafImg = loadImage('./assets/enemies/forest/leaf.png')
+	forestTiles = loadImage('./assets/enviroment/forestTiles2.png');
+	mountainTiles = loadImage('./assets/enviroment/mountainTiles.png');
+	coinsImg = loadImage('./assets/enviroment/coin.png');
+	flyImg = loadImage('./assets/enemies/forest/fly.png');
+	leafImg = loadImage('./assets/enemies/forest/leaf.png');
+
+	forestMusic = loadSound('./assets/sound/forest.ogg');
+	coinSound = loadSound('./assets/sound/coin.wav');
+
+	heartImg = loadImage('./assets/ui/heart.png');
 
 
 	initializeEnemies();
@@ -120,18 +175,32 @@ function setup() {
 
 	spawnLizard(spawner().x,spawner().y);
 	activePlayer = lizard;
+	//spawnFly()
 	//cube = new Sprite(lizard.x+30, lizard.y,6,6)
 	//console.log(lizard.x, lizard.y)
 
 	activePlayer.overlaps(spawnPoint);
 	activePlayer.overlaps(coins);	
-}
 
-function draw() {
+	//ui
+	ui = new Group();
+	for (let i = 0; i < 3; i++) {
+		heart = new ui.Sprite(30 + i * 40, 25, 19, 18, 'n');
+		heart.spriteSheet = heartImg;
+		heart.addAnis({
+			full: { row: 0, frames: 1, frameSize: [19,18]},
+			empty: { row: 1, frames: 1, frameSize: [19,18]}
+		});
+		heart.changeAni('full')
+	}
+
+	f = new fly.Sprite(lizard.x+100, lizard.y-50)
+}
+function update() {
 	clear();
 	if(gameState=='runGame'){
 
-		displayBackground(currentMap, 0);
+		displayBackground(forestBackground);
 
 		//if (hero.y>900) resetplayer();
 	
@@ -144,22 +213,32 @@ function draw() {
 		if(groundSensor.overlaps(spikes)) death();
 
 		//Change to next level
-		if(lizard.overlaps(endPoint)) changeLevel();
-
+		if(lizard.overlaps(endPoint)) endLevel();
+		
 		//collect coins
 		lizard.overlaps(coins)
 		keepScore();
 		chaseCheck();
+		//backgroundMusic(.0);
+
+		//ADD FALLING CHECK/ANIMATION												!!!
 	}
+}
 
-
-
+//Draws elements ignoring camera controll
+function drawFrame() {
+	camera.off();
+	ui.color = 'orange';
+	for (let i = 0; i < 9; i++) {
+		if (kb[i + 1]) ui[i].color = 'red';
+	}
+	ui.draw();
 }
 
 //keyboard/controller controlls
 function gameControlls(character){	
 	//----------Controls----------\\
-	if((character.currentState != character.states.ATTACK) && (character.currentState != character.states.BLOCK)){
+	if((character.currentState != character.states.ATTACK) && (character.currentState != character.states.BLOCK) && !inSequence){
 		stuckCheck();
 		if (kb.pressing('left')|| contro.leftStick.x < -0.25) {
 			character.mirror.x = true;
@@ -210,7 +289,7 @@ function gameControlls(character){
 		if (isOnGround()){
 			if(kb.presses('up')){
 				world.gravity.y = 15;
-				character.vel.y = -5
+				character.vel.y = -3.5;
 				character.changeAni(['jump','stand'])
 				world.gravity.y = 10
 			}
@@ -312,28 +391,32 @@ function releaseBlock(){
 
 //helper function: Grabs and returns coordinates from spawnPoint tile
 function spawner(){
-	let cords = {x: spawnPoint[0].position.x+24, y: spawnPoint[0].position.y-15}
-	return cords;
+	return {x: spawnPoint[0].position.x+24, y: spawnPoint[0].position.y-5};
 }
 
 //check if player touches a ground surface
 function isOnGround() {
 	return  groundSensor.overlapping(ground)||
-			groundSensor.overlapping(platform)
+			groundSensor.overlapping(platform)||
+			groundSensor.overlapping(cornerR)||
+			groundSensor.overlapping(cornerL)
 }
 
 //play death animation and reset player
 async function death() {
-		await lizard.changeAni(['death','dead']);
-		resetplayer();
-		lizard.changeAni('stand')
-
+	inSequence = true;
+	lizard.vel.x = 0;
+	await lizard.changeAni(['death','dead']);
+	resetplayer(resetCamera =false);
+	lizard.changeAni('stand')
+	inSequence = false;
 }
 
 //checks for coin collection and updates score
 function keepScore() {
 	for (let c of coins){
 		if (lizard.overlaps(c)){
+			coinSound.play();
 			c.remove();
 			score++;
 		}
@@ -369,3 +452,29 @@ function chaseCheck() {
 	}
 }
 
+//Plays background music according to level
+function backgroundMusic(volume){
+	switch(currentMap){
+		case 'forest':
+			forestMusic.play();
+			forestMusic.setVolume(volume);
+			break;
+		case 'mountain':
+			break;
+		case 'castle':
+			break;
+		case 'boss':
+			break;
+	}
+}
+
+//Handles reaching the end of current leve;
+async function endLevel() {
+	inSequence = true;
+	await lizard.changeAni('run');
+	await lizard.move(160, 'right', 1);
+	console.log('wait')
+	changeLevel();
+	lizard.changeAni('stand');
+	inSequence = false;
+}
