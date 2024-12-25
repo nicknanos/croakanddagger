5c551afdbeef42ff15e9ac83fe687f72cd7ea38c
Nick Nanos <nicknanos7@gmail.com>
2024-12-25 11:48:08 +0200

    Generalize background func| Fix constrain

--- game-code/camera.js
+++ game-code/camera.js
@@ -28,29 +28,21 @@ function canvasSetup() {
   }
   //Displays parallax background
   function displayBackground(level) {
-    switch (level) {
-      case 'forest':
-        for (b of forestBackground){
-          image(b.img, int(b.x), 0, canvas.w, canvas.h);
-          image(b.img, int(b.x)+ width, 0, canvas.w, canvas.h); // draw background to the right of the base one || int() resolves seams between 
-          image(b.img, int(b.x)- width, 0, canvas.w, canvas.h); // draw background to the left of the base one  || the instances of the background
-          if (activePlayer.currentState == activePlayer.states.WALK) b.x -= b.speed*direction+1*direction;
-          //if (direction != 0) b.x -= b.speed*direction+1*direction;
-          if (b.x < -width) b.x = 0;
-          }
-          //permanent cloud
-          //image(cloudImg, cloudX, 0, canvas.w, canvas.h);
-          //image(cloudImg, int(cloudX) + width, 0, canvas.w, canvas.h);
-          //if (cloudX < -width) cloudX = 0;
-          //cloudX -= 0.11;
-    }
+    for (b of level){
+      image(b.img, int(b.x), 0, canvas.w, canvas.h);
+      image(b.img, int(b.x)+ width, 0, canvas.w, canvas.h); // draw background to the right of the base one || int() resolves seams between 
+      image(b.img, int(b.x)- width, 0, canvas.w, canvas.h); // draw background to the left of the base one  || the instances of the background
+      if (activePlayer.currentState == activePlayer.states.WALK) b.x -= b.speed*direction+1*direction;
+      if (b.x < -width) b.x = 0;
+      }
   }
 
   //Sets limits for the camera
   function constrainCamera(tileGroup){
    //X Axi
-   let firstX = spawner().x;                             //x cord of spawner tile
-   let lastX = tileGroup[tileGroup.length-1].position.x; //x cord of last tile in map
+   let firstX = spawner().x + 30;                             //x cord of spawner tile
+   //let lastX = tileGroup[tileGroup.length-1].position.x; //x cord of last tile in map
+   let lastX = endPoint[0].position.x -30
    camera.x = constrain(cameraSensor.x, firstX, lastX);
    //Y Axis
    let firstY = tileGroup[0].position.y;                 //y cord of first tile in map
