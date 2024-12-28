c33f105f4b92b439ad6c25ea606dba383d9ed02a
Nick Nanos <nicknanos7@gmail.com>
2024-12-28 12:00:15 +0200

    Change walkable method |Add new level

--- game-code/levels.js
+++ game-code/levels.js
@@ -15,23 +15,46 @@ function changeLevel(){
         if (
             allSprites[i] != lizard &&
             allSprites[i] != groundSensor &&
-            allSprites[i] != cameraSensor
+            allSprites[i] != cameraSensor &&
+            allSprites[i] != leftSensor &&
+            allSprites[i] != rightSensor &&
+            allSprites[i] != topSensor
+
         ) {
             allSpritesGroup.add(allSprites[i]);
         }
     }
     allSpritesGroup.removeAll();
     //Create new level
+        //Changes which tileset is used depnding on the level
+//        if(currentLevel == 0){
+//            //setEnviroment(forestTiles);
+//        }else if(levels[currentLevel].map != levels[currentLevel - 1].map){ //Checks if the current level is different than the previous one
+//            console.log(levels[currentLevel].map)
+//            switch(levels[currentLevel].map){
+//                case 'forest':
+//	                myTiles.spriteSheet = forestTiles;
+//                    break;
+//                case 'mountain':
+//                    console.log('in')
+//                    myTiles.spriteSheet = mountainTiles;
+//                    myTiles.draw()
+//                case 'castle':
+//                    //ADD CASTLE TILES
+//                    break;
+//            }
+//        }
+
         //New Level Tiles
         tileGroup = new Tiles(levels[currentLevel].platforms, 0, 0, ground.w, ground.h);
         //Make deep copy of the tilegroup class instance
-        walkable = Object.create(Object.getPrototypeOf(tileGroup), Object.getOwnPropertyDescriptors(tileGroup));
-        
-        for (let t of tileGroup){
-            if(t.isWalkable == undefined){
-                walkable.splice(walkable.indexOf(t),1)
-            }
-        }
+        //walkable = Object.create(Object.getPrototypeOf(tileGroup), Object.getOwnPropertyDescriptors(tileGroup));
+        //
+        //for (let t of tileGroup){
+        //    if(t.isWalkable == undefined){
+        //        walkable.splice(walkable.indexOf(t),1)
+        //    }
+        //}
         //Change coin color
         for(let c of coins) c.changeAni(currentMap);
         //update map name
@@ -45,33 +68,64 @@ function changeLevel(){
 function preloadLevels() {
     levels.push({
         platforms: [
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b............................................",
-            "b........................<ggg>...............",
-            "b.....c....................rbl...............",
-            "b.....ff...................rbl...............",
-            "b............................................",
-            "b........fffff......<>.......................",
-            "bg>.................rb>...<gg>..<>...........",
-            "bbl.............<>..rbl..<bbbbggbl...........",
-            "bbl.....S.......rlnnbbbnnbbbbbbbbl....E......",
-            "bbl....gggggggggbbbbbbbbbbbbbbbbbbggggggggggg",
-            "bbbnnnnbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-            "b............................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................................................",
+"........................................................................................................................................................................................................................g...............................",
+"...............................................................................................1......c........................................c.......................................................................<b...............................",
+".....................................................................................................ff..............................fffffff...ffff..............................................................f....<bb...............................",
+"..........................................................................................fffffffff.................................................f................................................................<bbb..ffff.........................",
+"..................................................1..........................................................................................................................................................f......<bbbb...............................",
+"................................................................c.......................f.........................................c..................f...............1..........f....................c.............<bbbbb........ffff...................",
+"......c...................................<g>.......f..........fff......................................fff.......................f......................g...g...............g.....g.............ffffffff.........<bbbbbb...............................",
+"......f..............................fff..rbl..f.......f..<g>.......................fff...........................................1.....................<b...b>.............<b.....b>............................<bbbbbbb..............fff..............",
+".........fffff...............f.<g>........rbl.....f.......rbl......f.........................................f.........................................<bb...bb>...........<bb.....bb>..........................<bbbbbbbb...............................",
+"...............................rbb>.......rbl.............rbl....................f................c...................f.......f...f...f...............<bbb...bbb>.........<bbb.....bbb>........................<bbbbbbbbb...............................",
+"................<gg>.......ff..rbbb>......................rbl.......................f............c.c.................................................<bbbb...bbbb>.......<bbbb.....bbbb>......................<bbbbbbbbbb...............................",
+"..S......E......rbbl.........2.rbbbb>............2....2...rbl.............................2..g......................................................<bbbbbnnnbbbbb>.....<bbbbbnnnnnbbbbb>....................<bbbbbbbbbbb...............................",
+"ggggggggggggggggbbbbgggggggggggbbbbbbgggggggggggggggggggggbbbgggggggggggg...gggggggggggggggggbggg...ggggggggggggggggggggggggggggggggggggggggggggggggbbbbbbgggbbbbbbgggggbbbbbbbbbbbbbbbbbggggggggggggggggggggbbbbbbbbbbbbggggggggggggggggggggggggggggggg",
+"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbnnnbbbbbbbbbbbbbbbbbbbbbnnnbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
         ],
         map: "forest",
-        level: 0    
+        level: 0
+    });
+    levels.push({
+        platforms: [
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b................................................",
+            "b........................<ggg>...................",
+            "b.....c....................rbl...................",
+            "b.....ff...................rbl...................",
+            "b................................................",
+            "b........fffff......<>...........................",
+            "bg>.............1...rb>...<gg>..<>...............",
+            "bbl.............<>..rbl..<bbbbggbl...............",
+            "bbl....S........rlnnbbbnnbbbbbbbbl....E..........",
+            "bbl....gggggggggbbbbbbbbbbbbbbbbbbggggggggggggggg",
+            "bbbnnnnbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+            "b................................................",
+        ],
+        map: "mountain",
+        level: 1
     });
     levels.push({
         platforms: [
@@ -101,7 +155,7 @@ function preloadLevels() {
             "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
         ],
         map: "forest",
-        level: 1    
+        level: 2
     });
     
     currentLevel = levels[0].level;
