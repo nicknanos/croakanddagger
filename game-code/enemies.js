c5a3f3eaf41d408d6940980c59f9d1066d2a9088
Nick Nanos <nicknanos7@gmail.com>
2024-12-30 16:31:08 +0200

    Add new enemies

--- game-code/enemies.js
+++ game-code/enemies.js
@@ -51,6 +51,107 @@ function initializeEnemies(){
 	});
 
     for(l of leaf) l.changeAni('stand');
+
+	bat = new enemies.Group();
+	bat.w = 5;
+	bat.h = 5;
+	bat.layer = 5;
+	bat.collider = 'none'
+	bat.flying = true;
+	bat.rotationLock = 'true';
+	bat.spriteSheet = batImg;
+	bat.speed = 0.01;
+	bat.friction = 0;
+	bat.anis.w= 16;
+	bat.anis.h= 16;
+	bat.scale = 2;
+	bat.anis.offset.x = 1;
+	bat.anis.offset.y = -4.5;
+	bat.addAnis({
+		stand: { row: 0, frames: 5, frameDelay: 10 },
+		move: { row: 1, frames: 5, frameDelay: 10 },
+		attack: { row: 5, frames: 6, frameDelay: 13 },
+        death: { row: 2, frames: 5, frameDelay: 10},
+        dead: { row: 20, frames: 0}
+	});
+
+    for(b of bat) b.changeAni('stand');
+
+	cobra = new enemies.Group();
+	cobra.w = 9;
+	cobra.h = 7;
+	cobra.layer = 5;
+	cobra.flying = false;
+	cobra.rotationLock = 'true';
+	cobra.spriteSheet = cobraImg;
+	cobra.mass = 100;
+	cobra.speed = 0.01;
+	cobra.friction = 0;
+	cobra.anis.w= 32;
+	cobra.anis.h= 16;
+	cobra.scale = 2;
+	cobra.anis.offset.x = 2;
+	cobra.anis.offset.y = 0;
+	cobra.addAnis({
+		stand: { row: 0, frames: 8, frameDelay: 10 },
+		move: { row: 1, frames: 8, frameDelay: 10 },
+		attack: { row: 3, frames: 8, frameDelay: 13 },
+        death: { row: 2, frames: 8, frameDelay: 10},
+        dead: { row: 20, frames: 0}
+	});
+
+    for(c of cobra) c.changeAni('stand');
+
+	ghoul = new enemies.Group();
+	ghoul.w = 6;
+	ghoul.h = 10;
+	ghoul.layer = 5;
+	ghoul.flying = false;
+	ghoul.rotationLock = 'true';
+	ghoul.spriteSheet = ghoulImg;
+	ghoul.mass = 100;
+	ghoul.speed = 0.015;
+	ghoul.friction = 0;
+	ghoul.anis.w= 32;
+	ghoul.anis.h= 32;
+	ghoul.scale = 1.7;
+	ghoul.anis.offset.x = 0;
+	ghoul.anis.offset.y = -2.4;
+	ghoul.addAnis({
+		stand: { row: 0, frames: 4, frameDelay: 10 },
+		move: { row: 1, frames: 8, frameDelay: 10 },
+		attack: { row: 2, frames: 6, frameDelay: 13 },
+        death: { row: 4, frames: 6, frameDelay: 10},
+        dead: { row: 20, frames: 0}
+	});
+
+    for(g of ghoul) g.changeAni('stand');
+
+	imp = new enemies.Group();
+	imp.w = 6;
+	imp.h = 6;
+	imp.layer = 5;
+	imp.flying = false;
+	imp.rotationLock = 'true';
+	imp.spriteSheet = impImg;
+	imp.mass = 100;
+	imp.speed = 0.01;
+	imp.friction = 0;
+	imp.anis.w= 32;
+	imp.anis.h= 32;
+	imp.scale = 2;
+	imp.anis.offset.x = 0;
+	imp.anis.offset.y = -4.5;
+	imp.addAnis({
+		stand: { row: 0, frames: 7, frameDelay: 10 },
+		move: { row: 1, frames: 8, frameDelay: 10 },
+		attack: { row: 2, frames: 6, frameDelay: 13 },
+        death: { row: 4, frames: 6, frameDelay: 10},
+        dead: { row: 20, frames: 0}
+	});
+
+    for(i of imp) i.changeAni('stand');
+
 }
 
 //Constantly checks if enemy is near player
